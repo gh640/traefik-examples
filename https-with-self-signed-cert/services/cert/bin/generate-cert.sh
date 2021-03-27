e5a9ea388a6fe5d49d5a06e1f59d476ad642daae
Goto Hayato <habita.gh@gmail.com>
2021-03-27 16:10:55 +0900

    Add `https-with-self-signed-cert` example.

---
+++ https-with-self-signed-cert/services/cert/bin/generate-cert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
+  -keyout domain.key \
+  -out signed.crt \
+  -subj "/CN=${DOMAIN}" \
+  -addext "subjectAltName=DNS:${DOMAIN}" \
+  -addext "extendedKeyUsage=serverAuth"
