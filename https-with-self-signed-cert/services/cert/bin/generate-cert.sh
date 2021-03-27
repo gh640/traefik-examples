30739f96836f0f4387075a1ad3f3a08ce36d831f
GOTO Hayato <habita.gh@gmail.com>
2021-03-27 21:16:01 +0900

    Add inline comment in script `generate-cert.sh`.

--- https-with-self-signed-cert/services/cert/bin/generate-cert.sh
+++ https-with-self-signed-cert/services/cert/bin/generate-cert.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Generate a self-signed certificate with subjectAltName (SAN).
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
   -keyout domain.key \
   -out signed.crt \
